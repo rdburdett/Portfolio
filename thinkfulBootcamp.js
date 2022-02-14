07d2ccb65654f1fdcff41162d84edc9b0115b248
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2022-02-14 16:49:29 -0600

    create thinkfulBootcamp function

---
+++ thinkfulBootcamp.js
@@ -0,0 +1,16 @@
+let rob = {
+  name: "Robert Burdett",
+  occupation: "Registered Nurse",
+  skills: ["Nursing", "Music Production", "Snowboarding"]
+  }
+  
+function thinkfulBootcamp(student) {
+  student = {
+    ...student,
+    occupation: ["Software Developer"],
+    skills: [...student.skills, "Coding"]
+  }
+  return student
+}
+
+thinkfulBootcamp(rob)
